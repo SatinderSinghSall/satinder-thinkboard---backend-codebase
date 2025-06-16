815c54a2236879f216429a4bdfcc063de1bcb436
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-06-17 02:11:22 +0530

    Fixed deployment bug!

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 //! Middleware:
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://satinder-thinkboard-backend-codebase.onrender.com",
   })
 );
 app.use(express.json());
